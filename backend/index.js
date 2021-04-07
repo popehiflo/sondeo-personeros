552f6768648330137becb5e28bf9529c013052c9
popehiflo <poolpetterhijuel@hotmail.com>
2021-04-07 15:12:00 -0500

    Prueba conexion a base de datos

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 
+
+const { dbConnection } = require('./database/configuration');
+
 // Crear el servidor de express
 const app = express();
 
+// Base de Datos
+dbConnection();
 
 // Rutas
 app.use( '/api/personero', require('./routes/personero.routes'));
 app.listen(3000, () => {
     console.log('Servidor corriendo en puerto ' + 3000);
 })
-
-//mongodb+srv://personero_user:9IO0asG4XWg77Eff@cluster0.6by7g.mongodb.net/personerodb
-//personero_user
-//9IO0asG4XWg77Eff
