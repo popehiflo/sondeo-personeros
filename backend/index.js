aff6d8217598240392cd13cc44699ba73c2c91a5
popehiflo <poolpetterhijuel@hotmail.com>
2021-04-07 14:50:32 -0500

    Primera ruta/peticion

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,13 @@ const express = require('express');
 // Crear el servidor de express
 const app = express();
 
+
+// Rutas
+app.use( '/api/personero', require('./routes/personero.routes'));
 app.listen(3000, () => {
     console.log('Servidor corriendo en puerto ' + 3000);
 })
+
+//mongodb+srv://personero_user:9IO0asG4XWg77Eff@cluster0.6by7g.mongodb.net/personerodb
+//personero_user
+//9IO0asG4XWg77Eff
